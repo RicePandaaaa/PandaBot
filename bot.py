0116cfccb0678698d59becac2605ff4f5111aef4
RicePandaaa <anthony.ha.pham@gmail.com>
2022-07-01 15:44:06 -0500

    Added logging

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
-import asyncio, os, discord
+import asyncio, os, discord, logging
 from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
+
+logging.basicConfig(level=logging.INFO)
 
 bot = commands.Bot(intents=intents, command_prefix="!")
 
